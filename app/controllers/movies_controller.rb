60ef9f8ea091c6a8e51d5f6000c7fc5aabdfb413
Muhammad Umair <m.umair10@tamu.edu>
2016-10-12 23:49:52 +0000

    part 2 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,4 +1,6 @@
 class MoviesController < ApplicationController
+  
+  helper_method :chosen_rating?
 
   def movie_params
     params.require(:movie).permit(:title, :rating, :description, :release_date)
@@ -17,7 +19,44 @@ class MoviesController < ApplicationController
   def new
     # default: render 'new' template
   end
-
+  
+  def movie_params
+    params.require(:movie).permit(:title, :rating, :description, :release_date)
+  end
+  
+  
+  def index
+   # @movies = Movie.all
+    @all_ratings=Movie.get_all_ratings
+    
+    
+    if params[:sort_param] == 'title'
+      @movies = Movie.order(params[:sort_param])
+      @title_header = 'hilite'
+      return
+    elsif params[:sort_param] == 'release_date'
+      @movies = Movie.order(params[:sort_param])
+      @release_header= 'hilite'
+      return
+    end
+    
+    #session[:ratings] = params[:ratings] unless params[:ratings].nil?
+    #session[:order] = params[:order] unless params[:order].nil
+      
+    if !params[:ratings].nil? 
+        array_ratings = params[:ratings].keys
+        @movies = Movie.where(rating: array_ratings).order(params[:sort_param])
+      
+    else  
+      @movies=Movie.all
+      
+    end
+    
+    
+    
+  end
+  
+  
   def create
     @movie = Movie.create!(movie_params)
     flash[:notice] = "#{@movie.title} was successfully created."
@@ -41,5 +80,12 @@ class MoviesController < ApplicationController
     flash[:notice] = "Movie '#{@movie.title}' deleted."
     redirect_to movies_path
   end
+
+  def chosen_rating?(rating)
+    chosen_ratings = params[:ratings]
+    return true if chosen_ratings.nil?
+    chosen_ratings.include? rating
+  end
+
 
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,11 @@
 class Movie < ActiveRecord::Base
+    
+    def self.get_all_ratings
+ 	all_ratings = []
+		self.select(:rating).group(:rating).each do |mo|
+			all_ratings << mo.rating
+		end
+		return all_ratings
+  end
+    
 end
